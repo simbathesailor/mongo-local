4d4961e0271bd79fe65aa28ea8bfd8597fe0dfb3
simbathesailor <anilchaudhary453@gmail.com>
2020-08-09 19:28:33 +0530

    first code

---
+++ app.js
@@ -0,0 +1,25 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "myproject";
+
+// Create a new MongoClient
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+  assert.equal(null, err);
+
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
